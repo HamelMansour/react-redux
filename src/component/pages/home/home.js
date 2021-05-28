bcd14f6bf68b8587a066bf8811ce0b5bd8f5c545
HamelMansour <mansour.hamel@gmail.com>
2021-05-28 13:22:14 +0200

    test

--- src/component/pages/home/home.js
+++ src/component/pages/home/home.js
@@ -1,14 +1,22 @@
 import './home.css'
 import React from 'react';
 import { Router, useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { incrementerCounter } from '../../../store/store';
 
 const Home = () => {
-    const router = useHistory()
+    const dispatch = useDispatch ();
+    const counter= useSelector((state)=>state.counter.counter)
+    const increment = useCallback((counter)=> dispatch(incrementerCounter(counter)), [dispatch]);
 
+    const router = useHistory();
     return (
         <div>
             <h1>Home</h1>
             <h2 onClick={()=>router.push('login')}>GO TO LOGIN</h2>
+            <h2>cpt:{counter} </h2>
+            <h2 onClick={() => increment(counter + 1)}>Incrementer</h2>
         </div>
     )
 }
--- src/component/pages/login/login.js
+++ src/component/pages/login/login.js
@@ -1,14 +1,24 @@
 import './login.css'
 import React from "react";
 import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { incrementerCounter } from '../../../store/store';
+
 
 const Login = () => {
+    const dispatch = useDispatch ();
+    const counter= useSelector((state)=>state.counter.counter)
+    const increment = useCallback((counter)=> dispatch(incrementerCounter(counter)), [dispatch]);
+
     const router= useHistory()
 
     return (
         <div>
             <h1>Page login</h1>
             <h2 onClick={()=>router.push('home')}>GO TO Home</h2>
+            <h2>cpt:{counter} </h2>
+            <h2 onClick={() => increment(counter + 1)}>Incrementer</h2>
         </div>
     )
 }
